1993e43452f12945f73f2aa8968f5d911888239d
M-Serrano-Altena <Marc.Serrano.Altena@gmail.com>
2022-11-05 19:38:32 +0100

    Update sterrenkunde_practicum.bat

--- sterrenkunde_practicum.bat
+++ sterrenkunde_practicum.bat
@@ -1,7 +1,8 @@
 @echo off
 :loop
 
-"Path where your Python exe is stored\python.exe" "Path where your Python script is stored\sterrenkunde_practicum.py"
+:: "Path where your Python exe is stored\python.exe" "Path where your Python script is stored\sterrenkunde_practicum.py"
+"C:\Users\Marc_\anaconda3\python.exe" "C:\Users\Marc_\Documents\GitHub\Sterrenkunde-practicum\sterrenkunde_practicum.py"
 
 set /a loop=%loop%+1
 echo ...
