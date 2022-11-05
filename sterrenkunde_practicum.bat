bdefa4e1b540de921aabd0e246af66a2739817ff
M-Serrano-Altena <Marc.Serrano.Altena@gmail.com>
2022-11-05 18:36:17 +0100

    Update sterrenkunde_practicum.bat

--- sterrenkunde_practicum.bat
+++ sterrenkunde_practicum.bat
@@ -1,12 +1,12 @@
 @echo off
 :loop
 
-"Path where your Python exe is stored\python.exe" "Path where your Python script is stored\sterrenkunde_practicum.py"
+"C:\Users\Marc_\anaconda3\python.exe" "C:\Users\Marc_\Documents\GitHub\Sterrenkunde-practicum\sterrenkunde_practicum.py"
 
 set /a loop=%loop%+1
 echo ...
 set /p input= Type 'stop' om het programma te stoppen, klik enter om door te gaan:  
-if "%input%"=="stop" goto next
+if not x%input:stop=%==x%input% goto next
 goto loop
 
 :next
